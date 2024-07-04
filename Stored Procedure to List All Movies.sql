82060dee98369d41d1bedf01c7fd353505a037a1
Revan <104154992+RevanAlgh@users.noreply.github.com>
2024-07-04 21:10:49 +0300

    Update Stored Procedure to List All Movies.sql

--- Stored Procedure to List All Movies.sql
+++ Stored Procedure to List All Movies.sql
@@ -13,16 +13,3 @@ BEGIN
 END
 
 EXEC MoviesList;
-
-SELECT * FROM Authors;
-SELECT * FROM Country;
-
-SELECT M.MovieTitle, M.ImdbRating, M.YearReleased, M.Budget, M.BoxOffice, M.Language, DC.CompanyName, C.CountryName, STRING_AGG(A.AuthorName, ', ') AS Authors
-    FROM Movies M
-    JOIN DistributionCompanies DC ON M.DistributionCompanyID = DC.Id
-    JOIN Country C ON DC.CountryID = C.CountryID
-    LEFT JOIN AuthorMovie MA ON M.Id = MA.MovieID
-    LEFT JOIN Authors A ON MA.AuthorID = A.Id
-	GROUP BY M.MovieTitle, M.ImdbRating, M.YearReleased, M.Budget, M.BoxOffice, M.Language, DC.CompanyName, C.CountryName
-	ORDER BY M.MovieTitle;
-
